20b85648a93162c19ba2665ef1ded03ce7686e5f
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-06-20 19:27:30 +0200

    Add : helpers to require specified data from db

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\AppBundle\Controller;
 
 use AppBundle\Entity\Task;
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -86,5 +87,53 @@ class TaskControllerTest extends WebTestCase
         ]);
     }
 
+    public function logAsUser ()
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+        $this->client->submit($form, [
+            '_username' => 'user',
+            '_password' => 'demo'
+        ]);
+    }
+
+
+    //In case we want the task insert by the admin during the dunctinal
+    public function getInsertTask()
+    {
+        return $this->objectManager->getRepository(Task::class)->findOneBy(['title'=>'Tache de test']);
+    }
+
+    //In case we want the task insert by the user during the functional test
+    public function getInsertTaskFromUser()
+    {
+        return $this->objectManager->getRepository(Task::class)->findOneBy(['title'=>'Tache de test User']);
+    }
+
+    //In Case we want a random task
+    public function getRandomTask()
+    {
+        $tasks = $this->objectManager->getRepository(Task::class)->findAll();
+        return $tasks[array_rand($tasks)];
+    }
+
+    //In Case we want a random task from Admin
+    public function getAdminTask()
+    {
+        $user = $this->objectManager->getRepository(User::class)->findOneBy(['email'=>'admin@email.com']);
+        return $this->objectManager->getRepository(Task::class)->findOneBy(['user'=>$user->getId()]);
+    }
+
+    //In Case we want a random task from User
+    public function getUserTask()
+    {
+        $user = $this->objectManager->getRepository(User::class)->findOneBy(['email'=>'user@email.com']);
+        return $this->objectManager->getRepository(Task::class)->findOneBy(['user'=>$user->getId()]);
+    }
+
+    public function getAnonTask()
+    {
+        return $this->objectManager->getRepository(Task::class)->findOneBy(['user'=>null]);
+    }
 
 }
