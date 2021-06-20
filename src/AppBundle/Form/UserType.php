d2264d0cd866c1a010d5e8152fff82053ae3f0aa
RenardGris <73963127+RenardGris@users.noreply.github.com>
2021-06-20 16:03:33 +0200

    Add : role choiceType

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -3,11 +3,13 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -23,6 +25,21 @@ class UserType extends AbstractType
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class,
+                ['choices'  => array('Utilisateur' => 'ROLE_USER', 'Administrateur' => 'ROLE_ADMIN' ) ])
+        ;
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return $rolesString;
+                }
+            ))
         ;
     }
 }
